f5d21c77f46ea22c63bf6cac51aa8a938997b4a3
Theosaurus-Rex <theosaurus13@gmail.com>
2022-06-25 09:50:22 +1000

    Finish lesson 17

--- lib/watwitter/accounts/user_token.ex
+++ lib/watwitter/accounts/user_token.ex
@@ -134,6 +134,7 @@ defmodule Watwitter.Accounts.UserToken do
   end
 
   def user_and_contexts_query(user, [_ | _] = contexts) do
-    from t in Watwitter.Accounts.UserToken, where: t.user_id == ^user.id and t.context in ^contexts
+    from t in Watwitter.Accounts.UserToken,
+      where: t.user_id == ^user.id and t.context in ^contexts
   end
 end
